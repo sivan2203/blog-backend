6ce102d08f89fd218adfc4383e1b8d7b4bc700e2
sivan2203 <sivan9458@gmail.com>
2021-04-20 02:20:12 +0700

    add return jwt token

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -4,11 +4,23 @@ import { AuthService } from './service/auth.service';
 import { LocalStrategy } from './service/local.strategy';
 import { PassportModule } from '@nestjs/passport';
 import { AuthController } from './controller/auth.controller';
-
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [AdminModule, PassportModule],
+  imports: [
+    AdminModule,
+    PassportModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: (ConfigService: ConfigService) => ({
+        secret: ConfigService.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: ConfigService.get<string>('JWT_EXPIRES_IN') },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService, LocalStrategy]
+  providers: [AuthService, LocalStrategy],
 })
 export class AuthModule {}
--- src/modules/auth/service/auth.service.ts
+++ src/modules/auth/service/auth.service.ts
@@ -1,9 +1,13 @@
 import { AdminRepository } from '../../admin/service/admin.repository';
 import { Admin } from '../../admin/model/admin';
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 @Injectable()
 export class AuthService {
-  constructor(private adminRepository: AdminRepository) {}
+  constructor(
+    private adminRepository: AdminRepository,
+    private jwtService: JwtService,
+  ) {}
   async validateAdmin(login: string, pass: string): Promise<Admin | null> {
     const admin: Admin = await this.adminRepository.findByLogin(login);
     if (admin && admin.password === pass) {
@@ -11,5 +15,12 @@ export class AuthService {
       return secureAdmin;
     }
     return null;
+  }
+
+  async login(admin: Admin) {
+    const payload = { id: admin.id };
+    return {
+      accessToken: this.jwtService.sign(payload),
+    };
   }
 }
